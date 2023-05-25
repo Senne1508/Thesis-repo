e36e6e46ec8b94ed017ace770a768ee1c60035fa
Senne Daniëls <sennedaniels81@gmail.com>
2023-05-25 16:07:26 +0200

    Commiting all MATLAB programs

---
+++ MATLAB/Control method for line light/PWM.m
@@ -0,0 +1,22 @@
+function [f_PWM] = PWM(freq, offsetIndex, amp, duty, t)
+% Create PWM signal
+
+% freq [Hz]
+% offsetindex [# of indeces]
+% amp []
+% duty [%]
+% t [vector]
+
+f_PWM = amp*square(2*pi*freq.*t,duty);
+original_length = length(f_PWM);
+f_PWM = [zeros(1, offsetIndex), f_PWM];
+f_PWM = f_PWM(1:(original_length));
+
+
+% Logical indexing to find the -1 values
+idx = f_PWM == -1;
+
+% Replace the -1 values with zeros
+f_PWM(idx) = 0;
+
+end
